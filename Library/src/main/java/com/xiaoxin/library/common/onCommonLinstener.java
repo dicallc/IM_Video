16f11b636625ad47f835d6afe026b9ea2cecad04
jiangdk <jiangdk@e-lead.cn>
2018-04-10 10:55:24 +0800

    去除coldsleep

--- Library/src/main/java/com/xiaoxin/library/common/onCommonLinstener.java
+++
@@ -1,7 +0,0 @@
-package com.xiaoxin.library.common;
-
-public interface onCommonLinstener {
-        public void onSuccess(int code, Object result);
-
-        public void onFail(int code, String result);
-    }
